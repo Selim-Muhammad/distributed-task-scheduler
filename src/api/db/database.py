27b1746f4e013f9114e907e96d10ab76a0c7ec8a
Muhammad Selim <c24383413@mytudublin.ie>
2026-06-25 17:46:07 +0100

    Prepare application for deployment with environment variables

--- src/api/db/database.py
+++ src/api/db/database.py
@@ -1,7 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://admin:admin@127.0.0.1:5433/scheduler"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:admin@127.0.0.1:5433/scheduler"
+)
 
 engine = create_engine(DATABASE_URL)
 
--- src/api/queue/redis_client.py
+++ src/api/queue/redis_client.py
@@ -1,7 +1,14 @@
+import os
+
 import redis
 
+
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+
 redis_client = redis.Redis(
-    host="127.0.0.1",
-    port=6379,
+    host=REDIS_HOST,
+    port=REDIS_PORT,
     decode_responses=True
 )
